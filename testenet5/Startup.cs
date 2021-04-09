3b2ca5d2d2e21f95e4be8695a39df20da360962c
Ulisses Mendonca Junior <mendocs@gmail.com>
2021-04-09 16:30:15 -0300

    teste 4

--- testenet5/Startup.cs
+++ testenet5/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+
 
 namespace testenet5
 {
@@ -28,10 +28,7 @@ namespace testenet5
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "testenet5", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +37,7 @@ namespace testenet5
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "testenet5 v1"));
+
             }
 
             app.UseHttpsRedirection();
